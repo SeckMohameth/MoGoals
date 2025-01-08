848b8da7fc8d1ab198f360ac863268008d04f940
SeckMohameth <mohamethseck34@gmail.com>
2025-01-07 23:00:36 -0500

    Initial Commit

---
+++ MoGoals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoGoals
+//
+//  Created by Mohameth Seck on 1/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoGoals/MoGoalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoGoalsApp.swift
+//  MoGoals
+//
+//  Created by Mohameth Seck on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MoGoalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
